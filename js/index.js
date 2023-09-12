435e24acc6aa486db34d3850b37957bc747d3f4e
fatmadurak <fatmabadillidurak@gmail.com>
2023-09-12 14:52:52 +0300

    javascript menu click codes

---
+++ js/index.js
@@ -0,0 +1,60 @@
+const openMenuSidebar=document.querySelector("#openMenu");
+const sidebar=document.getElementById("menuSidebar");
+const closeMenuSidebar=document.querySelector("#closeMenu");
+const openSearchButton=document.getElementById("searchButton");
+const modalSearch=document.getElementById("modalSearch");
+const closeModalSearch=document.getElementById("closeSearchButton");
+
+
+openMenuSidebar.addEventListener("click",function(){
+
+
+
+sidebar.style.left="0%";
+
+
+})
+
+
+closeMenuSidebar.addEventListener("click",function(){
+
+
+
+    sidebar.style.left="-100%";
+    
+    
+    })
+
+
+    document.addEventListener("click",function(event){
+
+    
+    if (!event.composedPath().includes(sidebar)) {
+        
+         
+        sidebar.style.left="-100%";
+
+    }
+   
+       
+
+    })
+
+    openSearchButton.addEventListener("click",function(){
+
+
+       modalSearch.style.display="flex";
+
+        
+        
+        })
+
+        
+        closeModalSearch.addEventListener("click",function(){
+
+
+            modalSearch.style.display="none";
+     
+             
+             
+             })
