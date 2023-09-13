fe5773113f1d6a8bab06207bc8f60b08e4b16846
fatmadurak <fatmabadillidurak@gmail.com>
2023-09-13 08:56:55 +0300

    product-item added glide.js library

--- js/glide.js
+++ js/glide.js
@@ -8,3 +8,38 @@ new Glide('.glide', {
           // Slayt arasındaki boşluğu belirtin (px cinsinden).
    
   }).mount();
+
+
+  new Glide('.glide2', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    hoverpause: true,
+    breakpoints: {
+      768: {
+        perView: 2 // 768 piksel genişlikte 2 slayt göster
+      },
+      576: {
+        perView: 1 // 576 piksel genişlikte 1 slayt göster
+      }
+      // İstediğiniz kadar breakpoint ekleyebilirsiniz
+    }
+  }).mount();
+
+  new Glide('.glide3', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    autoplay: 2000,
+    hoverpause: true,
+    animationDuration: 1000,
+    breakpoints: {
+      768: {
+        perView: 2 // 768 piksel genişlikte 2 slayt göster
+      },
+      576: {
+        perView: 1 // 576 piksel genişlikte 1 slayt göster
+      }
+      // İstediğiniz kadar breakpoint ekleyebilirsiniz
+    }
+  }).mount();
