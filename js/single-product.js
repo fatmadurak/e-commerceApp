cb2ec322eebd8a36d6fa16040a7c5a096e3626ee
fatmadurak <fatmabadillidurak@gmail.com>
2023-09-22 13:09:34 +0300

    singleproduct js add to cart codes

--- js/single-product.js
+++ js/single-product.js
@@ -1,4 +1,4 @@
-
+import {updateCartCount} from "./index.js"
 
 const productId=localStorage.getItem("product-id")?JSON.parse(localStorage.getItem("product-id")):localStorage.setItem("product-id",JSON.stringify(1));
 
@@ -36,9 +36,45 @@ const glideImage=document.querySelector(".glideImage");
 
 findProduct.img.thumbs.forEach((imageUrl, index) => {
     glideImage.src = imageUrl;
-    console.log(glideImage.src);
+  
    
   });
+
+//addto cart
+
+
+const quantityDom=document.querySelector("#quantity");
+const addtocartDom=document.querySelector("#addtoProduct");
+
+
+let cartIn=localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")):[];
+
+
+if (cartIn=[]) {
+  addtocartDom.addEventListener("click",function(e){
+
+    e.preventDefault();
+  
+    cartIn.push({...findProduct,quantity:Number(quantityDom.value)})
+   
+    localStorage.setItem("cart",JSON.stringify(cartIn));
+    updateCartCount();
+    addtocartDom.setAttribute("disabled","disabled");
+  
+  })
+  
+}
+
+else{
+
+
+  addtocartDom.setAttribute("disabled","disabled");
+
+
+}
+
+
+
 
 
   
